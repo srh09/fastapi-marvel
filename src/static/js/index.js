aa144f46689c2670f7fb69e3fc96c9b25c087b2c
Reid Holand <reid.holand@onsharp.com>
2022-05-19 22:08:58 -0500

    Setup static files

---
+++ src/static/js/index.js
@@ -0,0 +1,9 @@
+const sayHi = () => console.log('Hi there------')
+const sayBye = () => console.log('goodbye-----')
+
+let mCharacter = {
+    'id': 12345,
+    'name': 'Spectrum',
+    'description': 'Nice and thorough description of Spectrum',
+    'picture': null
+}
