37fcc084f903e629696858b80842652a7e837295
Reid Holand <reid.holand@onsharp.com>
2022-05-19 22:08:44 -0500

    Housekeeping

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 case $1 in
+    'local') python ./src/main.py;;
     'build') docker compose -p fastapi build;;
     'start') docker compose -p fastapi up web;;
     'stop') docker compose -p fastapi down;;
--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
-from src.api.handlers import test
+from fastapi.staticfiles import StaticFiles
+from api.handlers import test
 
-app = FastAPI()
+app = FastAPI(
+    root_path='/src'
+)
 
-app.include_router(test.router, prefix='/api/v1/test')
+app.mount("/", StaticFiles(directory="src/static", html=True), name="static")
+app.include_router(test.router, prefix='/api/v1/test/t2')
+
+
+@app.get("/app")
+def read_main(request):
+    return {"message": "Hello World", "root_path": request.scope.get("root_path")}
+
 
 if __name__ == '__main__':
-    uvicorn.run('src.main:app', reload=True)
+    uvicorn.run('main:app', reload=True)
