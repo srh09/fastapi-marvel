8c3c2695ea28efca4c7a2993fa3278be1d125957
SRH <srholand@gmail.com>
2022-06-10 21:24:37 -0500

    update shell script

--- bin/run.sh
+++ bin/run.sh
@@ -12,5 +12,13 @@ case $1 in
     psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'TRUNCATE comic CASCADE';
     psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'TRUNCATE character_comic';
     ;; 
-    *) echo 'Invalid selection.  Options: build | start'
+    'drop')
+    psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'DROP TABLE character CASCADE';
+    psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'DROP TABLE comic CASCADE';
+    psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'DROP TABLE character_comic';
+    psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'DROP TABLE alembic_version';
+    rm -r ./alembic/versions/*
+    ;;
+    ## rm -rf
+    *) echo 'Invalid selection.  Options: local | generate | migrate | build | start | stop | truncate | drop'
 esac
